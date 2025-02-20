8099695b06605e862b755359927ae9980707cefd
pepe <franck.calestroupat@gmail.com>
2025-02-20 22:08:18 +0100

    Modification du chemin où sont stockés les sons. Gestion du volume du son qui était trop fort.

--- src/main/java/com/francky/projet/maze_Grok/utils/SoundManager.java
+++ src/main/java/com/francky/projet/maze_Grok/utils/SoundManager.java
@@ -1,26 +1,31 @@
 package com.francky.projet.maze_Grok.utils;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundManager {
-    private Clip backgroundClip; // Pour la musique de fond
-    private static final String SOUND_PATH = "src/com/francky/projet/maze_Grok/sounds/";
+	private Clip backgroundClip;
 
-    // Charger et jouer la musique de fond en boucle
+    // Charger et jouer la musique de fond en boucle avec volume ajusté
     public void playBackgroundMusic(String fileName) {
         try {
-            File file = new File(SOUND_PATH + fileName);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(
+                getClass().getResource("/com/francky/projet/maze_Grok/sounds/" + fileName)
+            );
             backgroundClip = AudioSystem.getClip();
             backgroundClip.open(audioStream);
-            backgroundClip.loop(Clip.LOOP_CONTINUOUSLY); // Boucle infinie
+            
+            // Ajuster le volume (gain en dB)
+            FloatControl gainControl = (FloatControl) backgroundClip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-20.0f); // Réduit le volume de 20 dB (ajustable)
+            
+            backgroundClip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
@@ -34,14 +39,15 @@ public class SoundManager {
         }
     }
 
-    // Jouer un effet sonore (déplacement ou victoire)
+    // Jouer un effet sonore (sans modification du volume ici)
     public void playSoundEffect(String fileName) {
         try {
-            File file = new File(SOUND_PATH + fileName);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(
+                getClass().getResource("/com/francky/projet/maze_Grok/sounds/" + fileName)
+            );
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
-            clip.start(); // Joue une seule fois
+            clip.start();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
